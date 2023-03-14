6e56cdf063398a15d7aa0f9a4fa679efac4074c4
IVLyubarskij <ivanred289@gmail.com>
2023-03-14 11:06:14 +0300

    Update project structure

--- Assets/Code/Code_AFedorov/Programmer.cs
+++
@@ -1,7 +0,0 @@
-class Programmer {
-    var last_name = "Федоров";
-    var name = "Алексей";
-    var name_by_father = "Алексеевич";
-    var group = "М8О-109Б-22";
-    var team = "VRoom";
-}
--- Assets/Code/Code_IVLyubarskij/Programmer.cs
+++
@@ -1,7 +0,0 @@
-class Programmer {
-    var last_name = "Любарский";
-    var name = "Иван";
-    var name_by_father = "Владимирович";
-    var group = "М8О-109Б-22";
-    var team = "VRoom";
-}
